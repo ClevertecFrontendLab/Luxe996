98931a9704c7f16bb1c021daf304f91634585ac4
AndreyTverdovskiy <tverdovskiyandrey1996@gmail.com>
2024-02-13 20:41:06 +0300

    fix:change layout main page

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,23 +1,30 @@
-import {Layout} from "antd";
+import {Button, Layout} from "antd";
 import s from './main-page.module.scss';
 import {NavBar} from "@components/nav-bar";
 import {Switcher} from "@components/nav-bar/switcher";
 import AppHeader from "@components/layout/app-header/app-header";
+import useBreakpoint from 'antd/es/grid/hooks/useBreakpoint';
 import {useState} from "react";
 import {ButtonMenu} from "@components/nav-bar/button-menu/button-menu";
+import {AppContent} from "@components/app-content/app-content";
+import {AppCard} from "@components/app-card/app-card";
+import {CalendarOutlined, HeartFilled, IdcardOutlined} from "@ant-design/icons";
+import {FooterCard} from "@components/footer-card/footer-card";
 
 const {Header, Footer, Sider, Content} = Layout;
 
 export const MainPage: React.FC = () => {
     const [collapsed, setCollapsed] = useState(true);
+    const breakpoint = useBreakpoint();
 
     const collapseHandler = () => setCollapsed((pervState) => !pervState)
 
     return (
         <Layout className={s.container}>
             <Sider
+                className={s.nav}
                 width={208}
-                collapsedWidth={64}
+                collapsedWidth={`${breakpoint.xs ? 0 : 64}`}
                 trigger={null}
                 collapsible
                 collapsed={collapsed}
@@ -30,8 +37,26 @@ export const MainPage: React.FC = () => {
                 <Header>
                     <AppHeader/>
                 </Header>
-                <Content>Content</Content>
-                <Footer>Footer</Footer>
+                <Content>
+                    <AppContent>
+                        <AppCard title={'Расписать тренировки'} link={'Тренировки'} icon={<HeartFilled />}/>
+                        <AppCard title={'Назначить календарь'} link={'Календарь'} icon={<CalendarOutlined />}/>
+                        <AppCard title={'Заполнить профиль'} link={'Профиль'} icon={<IdcardOutlined />}/>
+                    </AppContent>
+                </Content>
+                <Footer>
+                    <div className={s.footer}>
+                        <div>
+                            <Button type={'link'} size={'large'}>
+                                Смотреть отзывы
+                            </Button>
+                        </div>
+
+                        <div className={s['footer-card']}>
+                            <FooterCard/>
+                        </div>
+                    </div>
+                </Footer>
             </Layout>
         </Layout>
     );
