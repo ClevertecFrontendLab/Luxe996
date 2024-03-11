7fb55ae1878ef64f708f17a656d4d9bafa8ce160
AndreyTverdovskiy <tverdovskiyandrey1996@gmail.com>
2024-03-11 23:48:52 +0300

    fix: add PropsWithChildren type

--- src/components/nav-bar/button-menu/button-menu.tsx
+++ src/components/nav-bar/button-menu/button-menu.tsx
@@ -1,16 +1,12 @@
 import { Button } from 'antd';
-import { ReactNode } from 'react';
+import { PropsWithChildren } from 'react';
 import s from './button-menu.module.scss';
 import ExitIcon from '@public/exit-menu.svg?react';
 import { loginAC } from '@redux/reducers/auth-reducer';
 import { useAppDispatch } from '@hooks/typed-react-redux-hooks';
 import useBreakpoint from 'antd/es/grid/hooks/useBreakpoint';
 
-type ButtonMenuProps = {
-    children: ReactNode;
-};
-
-export const ButtonMenu = ({ children }: ButtonMenuProps) => {
+export const ButtonMenu = ({ children }: PropsWithChildren) => {
     const dispatch = useAppDispatch();
     const breakpoint = useBreakpoint();
     const onClick = () => {
